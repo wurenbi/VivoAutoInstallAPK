2bdf5e546bee6ad2e1b8d505bfa48a4f7ec93b0a
S <wurenbi@sina.com>
2020-08-27 15:44:32 +0800

    some log

--- app/src/main/java/com/jianwu/vivoautoinstallapk/InstallerHelperService.java
+++ app/src/main/java/com/jianwu/vivoautoinstallapk/InstallerHelperService.java
@@ -3,18 +3,36 @@ package com.jianwu.vivoautoinstallapk;
 import android.accessibilityservice.AccessibilityService;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
+
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class InstallerHelperService extends AccessibilityService {
+    public static final Set<String> EVENT_PACKAGE = new HashSet<>();
+    public static final Set<String> EDITTEXT_PACKAGE = new HashSet<>();
+    public static final Set<String> EDITTEXT_CLASSNAME = new HashSet<>();
+
+    static {
+        EVENT_PACKAGE.add("com.vivo.secime.service");
+
+        EDITTEXT_PACKAGE.add("com.bbk.account");
+
+        EDITTEXT_CLASSNAME.add("android.widget.EditText");
+    }
+
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
         AccessibilityNodeInfo rootNode = getRootInActiveWindow();
+        Log.w("InstallerHelperService", "onAccessibilityEvent rootNode is null!");
         if (rootNode == null) return;
 
-        if (event.getPackageName().equals("com.vivo.secime.service")) {
+        if (EVENT_PACKAGE.contains(event.getPackageName().toString())) {
+            Log.w("InstallerHelperService", "event.getPackageName() is " + event.getPackageName());
             //vivo账号密码
             String password = (String) SharePreferencesUtils.getParam(getApplicationContext(),
                     AppConstants.KEY_PASSWORD, "");
@@ -30,10 +48,14 @@ public class InstallerHelperService extends AccessibilityService {
      */
     private void fillPassword(AccessibilityNodeInfo rootNode, String password) {
         AccessibilityNodeInfo editText = rootNode.findFocus(AccessibilityNodeInfo.FOCUS_INPUT);
+        Log.w("InstallerHelperService", "fillPassword editText is null!");
         if (editText == null) return;
 
-        if (editText.getPackageName().equals("com.bbk.account")
-                && editText.getClassName().equals("android.widget.EditText")) {
+        if (EDITTEXT_PACKAGE.contains(editText.getPackageName().toString())
+                && EDITTEXT_CLASSNAME.contains(editText.getClassName().toString())) {
+            Log.w("InstallerHelperService",
+                    "editText.getPackageName() is " + editText.getPackageName() + ","
+                            + "editText.getClassName() is " + editText.getClassName());
             Bundle arguments = new Bundle();
             arguments.putCharSequence(AccessibilityNodeInfo
                     .ACTION_ARGUMENT_SET_TEXT_CHARSEQUENCE, password);
